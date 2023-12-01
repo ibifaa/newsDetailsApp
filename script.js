98d2b978f876d80f469a33afb44bf27c2542d03a
devibifaa <ib2link@yahoo.com>
2023-12-01 06:01:40 +0100

    basic structure

---
+++ script.js
@@ -0,0 +1,25 @@
+class news{
+      const (name, url, author){
+        this.name = name;
+        this.url = url;
+        this.author = author;
+    }
+
+    newDetails = {
+        name: this.name,
+        url: this.url,
+        author: this.author
+    }
+
+    addLink(){
+
+    }
+
+    removeLink(){
+
+    }
+
+    quit(){
+
+    }
+}
